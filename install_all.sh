35143389daeb0c4b591fd5030c83a07f6ade0be2
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-03-10 22:42:11 -0300

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -28,8 +28,11 @@ MSG=$(echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check)
 if [ "$MSG" == "kubectl: OK" ]; then
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     kubectl version --client
+    echo 'source <(kubectl completion bash)' >>~/.bashrc
+    source ~/.bashrc
 else
     echo "WARNING: 1 computed checksum did NOT match."
+    exit
 fi
 
 echo "Installing minikube ..."
