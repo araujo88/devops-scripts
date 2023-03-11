ccec43eeec939a626842d9d247da4e9cf9e248c1
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-03-10 22:36:32 -0300

    Create install_kubectl.sh

---
+++ install_kubectl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing kubectl ..."
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+MSG=$(echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check)
+
+if [ "$MSG" == "kubectl: OK" ]; then
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version --client
+else
+    echo "WARNING: 1 computed checksum did NOT match."
+fi
