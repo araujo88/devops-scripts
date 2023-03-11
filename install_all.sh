e305eeb99092d2ece4f27e765c9aedc6cf9d7af3
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-03-10 22:39:11 -0300

    Create install_all.sh

---
+++ install_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Installing docker ..."
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+docker run hello-world
+
+echo "Installing kubectl ..."
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+MSG=$(echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check)
+
+if [ "$MSG" == "kubectl: OK" ]; then
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version --client
+else
+    echo "WARNING: 1 computed checksum did NOT match."
+fi
+
+echo "Installing minikube ..."
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=docker
+minikube config set driver docker
+kubectl cluster-info
