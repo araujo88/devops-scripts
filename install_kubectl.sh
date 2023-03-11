c97b6c240111dee6905b0fc751d9165cc972eebf
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-03-10 22:41:07 -0300

    Update install_kubectl.sh

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -10,6 +10,8 @@ MSG=$(echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check)
 if [ "$MSG" == "kubectl: OK" ]; then
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     kubectl version --client
+    echo 'source <(kubectl completion bash)' >>~/.bashrc
+    source ~/.bashrc
 else
     echo "WARNING: 1 computed checksum did NOT match."
 fi
