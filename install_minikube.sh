a219924a2a8fa92742cdd75268d9b5235b564e8e
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-03-10 22:38:44 -0300

    Create install_minikube.sh

---
+++ install_minikube.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing minikube ..."
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=docker
+minikube config set driver docker
+kubectl cluster-info
